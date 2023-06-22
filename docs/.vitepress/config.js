e5a745ba42cd2e69bd09062c9722db74e04e689e
liruixiang <3054809485@qq.com>
2023-06-22 16:09:22 +0800

    第一次提交

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  title: "lrx's Blog",
+  base: '/Blog/',
+  description: 'Just playing around.',
+  themeConfig: {
+      logo:'/1.png',
+      nav: [
+          {
+              text: '作品集',
+              link:'/artworks/'
+          },
+          {
+              text:'基础知识',
+              items:[
+               { text:'html',link:'/base/html/'},
+               { text:'css',link:'/base/css/'},
+               { text:'js',link:'/base/js/'},
+               { text:'ts',link:'/base/ts/'}
+              ]
+          },
+          {
+              text:'vue3',
+              link:'/vue3/'
+          },
+          {
+              text:'需求杂症',
+              link:'/xuqiuzazhen/'
+          },
+          {
+            text:'每周总结',
+            link:'/weeks/'
+          },
+          {
+            text:'Leetcode',
+            link:'/leetcode/'
+          }
+      ],
+      // sideber以对象的形式配置的话每个sidebar都是独立的 如果以数组的形式配置那么侧边栏是公共的 进入其他子页面都可以看到
+      sidebar: {
+           collapsed: false, 
+            //是否折叠
+          '/work/': [
+              {
+                  text: '我的工作日常',
+                  items: [
+                      { text: '踩坑记录', link: '/work/mistake' },
+                      { text: '学习记录', link: '/work/study' }
+                  ]
+              },
+              {
+                text:'我的开源项目',
+                items:[
+                  {text:'...1', link:'/work/opensource'},
+                ]
+              }
+          ]
+      }
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,35 @@
+---
+layout: home
+ 
+hero:
+  name: Lrx's的前端旅途
+  text: 
+  tagline: 努力努力再努力💪！
+  image: /1.png
+  actions:
+    - theme: brand
+      text: 让我康康
+      link: /guide/what-is-vitepress
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/vuejs/vitepress
+ 
+features:
+  - icon: 🙋‍
+    title: 前端基础知识
+    details: Typescript 前端工程化...
+  - icon: 📚
+    title: 前端工作日常
+    details: 开发工具使用 踩坑记录...
+  - icon: 🛠️
+    title: 学习总结
+    details: Vue3 leetcode...
+---
+<style>
+    :root {
+  --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #f55a3e, #7c9eb7);
+  --vp-home-hero-image-background-image: linear-gradient( -45deg, #f55a3e 50%, #7c9eb7 50% );    
+  --vp-home-hero-image-filter: blur(40px);
+}
+</style>
